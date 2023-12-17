f0714d6355358ed1d6c475f5a8ba3e2d5ab7dfbd
sarna320 <sarnacki99@wp.pl>
2023-12-17 15:33:40 +0100

    changed events.py to get all links and then from links extract info

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from events import find_all_events
+from events import find_events_live_and_today, find_events_next
 from leagues import find_leagues
 
 leagues = find_leagues()
-events = find_all_events(leagues["link"][0])
 for i in range(len(leagues["name"])):
-    print(f'League:{leagues["name"][i]} link: {leagues["link"][i]}')
+    print(f'{i}:{leagues["name"][i]} link: {leagues["link"][i]}')
+
 print("")
-for i in range(len(events["home"])):
-    print(f'{events["home"][i]} vs {events["away"][i]} link: {events["link"][i]}')
+print("Live and today events")
+events_live_and_today = find_events_live_and_today(leagues["link"][0])
+for i in events_live_and_today:
+    print(i)
+
+print("")
+print("Future events:")
+events_next = find_events_next(leagues["link"][0])
+for i in events_next:
+    print(i)
+print("")
